a9c137cdf211dd339ab888621bb9d41881fd3dd8
TonPKG <piyaphat_kh@cmu.ac.th>
2022-08-04 16:21:59 +0700

    lab 05 completed

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  if (inputAdd.value == "") {
+    alert("To do cannot be empty");
+    return;
+  }
+  addTodo(inputAdd.value, false);
+  inputAdd = "";
+  saveTodo();
 };
 
 function addTodo(title, completed) {
@@ -23,13 +30,42 @@ function addTodo(title, completed) {
   doneBtn.innerText = "Done";
   doneBtn.className = "btn btn-success me-2";
 
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "line-through")
+      span.style.textDecoration = "";
+    else span.style.textDecoration = "line-through";
+    saveTodo();
+  };
+
   //create delete button
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Delete";
   deleteBtn.className = "btn btn-danger";
 
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
+
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+
   //your code here
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+  todoCtn.prepend(div);
   //append todo to HTML...
+
   //define buttons event...
 }
 
@@ -37,11 +73,21 @@ function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
   }
+  localStorage.setItem("items", JSON.stringify(data));
   //your code here
 }
 
 function loadTodo() {
+  const data = JSON.parse(localStorage.getItem("items"));
+  for (let i = data.length - 1; i >= 0; i--) {
+    addTodo(data[i].title, data[i].completed);
+  }
   //your code here
 }
 
